cc7706bbfec592b1b0da39859cf436bd00ed8b56
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-08-02 23:01:20 -0300

    Project created

---
+++ src/main/java/com/paulo/simplified_wallet/SimplifiedWalletApplication.java
@@ -0,0 +1,13 @@
+package com.paulo.simplified_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulo/simplified_wallet/SimplifiedWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulo.simplified_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
