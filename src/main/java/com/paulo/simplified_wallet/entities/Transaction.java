0605616f2a8404474a0b122c3f99e8a0be6a6e4d
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-08-05 13:09:42 -0300

    refactor: add builder annotation

--- src/main/java/com/paulo/simplified_wallet/entities/Transaction.java
+++ src/main/java/com/paulo/simplified_wallet/entities/Transaction.java
@@ -1,10 +1,7 @@
 package com.paulo.simplified_wallet.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -15,6 +12,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Entity
 @Table(name = "tb_transaction")
+@Builder
 public class  Transaction {
 
     @Id
