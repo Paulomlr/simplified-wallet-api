9a87b00034190bbb2f813d2eb388b05b872158e3
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-08-05 12:54:14 -0300

    feat: add ClientNotification interface

---
+++ src/main/java/com/paulo/simplified_wallet/controllers/ClientNotification.java
@@ -0,0 +1,11 @@
+package com.paulo.simplified_wallet.controllers;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(url = "https://util.devi.tools/api/v1/notify", name = "notification")
+public interface ClientNotification {
+
+    @PostMapping
+    void sendNotification();
+}
